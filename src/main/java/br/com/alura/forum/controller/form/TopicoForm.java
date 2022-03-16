5765e96c5d7a1916fc0f92024f76518d4394caa4
Gustavo Henrique Amaro <gustavohenrique.miranda@hotmail.com>
2022-03-16 13:46:00 -0300

    Trabalhando com Post

---
+++ src/main/java/br/com/alura/forum/controller/form/TopicoForm.java
@@ -0,0 +1,43 @@
+package br.com.alura.forum.controller.form;
+
+import br.com.alura.forum.modelo.Curso;
+import br.com.alura.forum.modelo.Topico;
+import br.com.alura.forum.repository.CursoRepository;
+;
+
+public class TopicoForm {
+
+	private String titulo;
+	private String messagem;
+	private String nomeCurso;
+
+	public String getTitulo() {
+		return titulo;
+	}
+
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+
+	public String getMessagem() {
+		return messagem;
+	}
+
+	public void setMessagem(String messagem) {
+		this.messagem = messagem;
+	}
+
+	public String getNomeCurso() {
+		return nomeCurso;
+	}
+
+	public void setNomeCurso(String nomeCurso) {
+		this.nomeCurso = nomeCurso;
+	}
+
+	public Topico converter(CursoRepository cursoRepository) {
+		Curso curso = cursoRepository.findByNome(nomeCurso); 
+		return new Topico(titulo,messagem,curso); 
+	}
+
+}
